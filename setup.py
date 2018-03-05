0c649d7ffafdfb2f7f8c3802cb110bbc0920ee7f
Henke Adolfsson <catears@catears.se>
2018-03-05 07:50:32 +0100

    Re add setup things

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='kattcmd',
+    packages=['kattcmd'],
+    version='0.0.1',
+    description='Kattis solution management CLI written in python',
+    author='Henrik Adolfsson',
+    author_email='anting004@gmail.com',
+    url='https://git.lysator.liu.se/catears/kattis-command',
+    download_url='httsp://git.lysator.liu.se/catears/kattis-command/master/archive.tar.gz',
+    keywords=['kattis', 'cli', 'competitive programming'],
+    classifiers=[]
+)
---
+++ test/core.py
@@ -0,0 +1,3 @@
+import os
+import kattcmd.core
+
