2e628acb4f45974ae6b079ddd50e90360669b5ec
Henke Adolfsson <catears@catears.se>
2018-03-07 16:40:10 +0100

    Check that a problem exists before opening it. No force option

--- kattcmd/commands/open.py
+++ kattcmd/commands/open.py
@@ -1,28 +1,46 @@
 import os
 import click
+import requests
+
+
+def DoesProblemExist(problemname):
+    '''Checks against kattis that a problem exists.'''
+    url = 'https://open.kattis.com/problems/{}'.format(problemname)
+    response = requests.get(url)
+    return response.status_code == 200
 
 
 def OpenProblem(bus, problemname):
     '''Opens the problem inside the kattis folder.'''
     root = bus.call('kattcmd:find-root', bus)
     problem_path = os.path.join(root, 'kattis', problemname)
-    if not os.path.isdir(problem_path):
+
+    if not DoesProblemExist(problemname):
+        bus.call('kattcmd:open:problem-doesnt-exist', problemname)
+
+    elif os.path.isdir(problem_path):
+        bus.call('kattcmd:open:problem-already-opened', problem_path)
+
+    else:
         os.mkdir(problem_path)
         bus.call('kattcmd:open:problem-opened', problem_path)
-    else:
-        bus.call('kattcmd:open:problem-already-opened', problem_path)
 
 
 def Init(bus):
-    def on_problem_opened(path):
+
+    def OnProblemOpened(path):
         '''Event called when a problem is opened.'''
 
-    def on_problem_already_opened(path):
+    def OnProblemAlreadyOpened(path):
         '''Event called when the problem already exists.'''
 
+    def OnProblemDoesntExist(problem):
+        '''Event called when a problem does not exist'''
+
     bus.provide('kattcmd:open', OpenProblem)
-    bus.provide('kattcmd:open:problem-opened', on_problem_opened)
-    bus.provide('kattcmd:open:problem-already-opened', on_problem_already_opened)
+    bus.provide('kattcmd:open:problem-opened', OnProblemOpened)
+    bus.provide('kattcmd:open:problem-already-opened', OnProblemAlreadyOpened)
+    bus.provide('kattcmd:open:problem-doesnt-exist', OnProblemDoesntExist)
 
 
 def CLI(bus, parent):
@@ -42,12 +60,17 @@ def CLI(bus, parent):
     def OnTestFail(response):
         click.echo('Could not automatically download tests.')
 
+    def OnProblemDoesntExist(problem):
+        click.echo('Could not find problem {}'.format(problem))
+        exit(0)
+
     @click.command()
     @click.argument('name')
     def open(name):
         bus.listen('kattcmd:open:problem-opened', OnProblemOpen)
         bus.listen('kattcmd:testdownload:downloaded', OnTestsLoaded)
         bus.listen('kattcmd:testdownload:bad-status', OnTestFail)
+        bus.listen('kattcmd:open:problem-doesnt-exist', OnProblemDoesntExist)
 
         bus.call('kattcmd:open', bus, name)
         bus.call('kattcmd:testdownload', bus, name)
--- test/test_open.py
+++ test/test_open.py
@@ -23,3 +23,26 @@ def test_OpenNewProblem(bus):
 
     should_exist = os.path.join(home, 'kattis', problemname)
     assert os.path.exists(should_exist)
+
+
+@WithCustomCWD
+@WithModules([init, root, open])
+def test_OpenNonExistantProblem(bus):
+    # Random string, that has a very little risk of being a problem
+    problemname = '784c114ff7c9865cc26f69cc05868a6e9af3a4fdd2a6b6f2331'
+    home = os.environ['HOME']
+
+    checker = CallChecker()
+    bus.listen('kattcmd:init:directory-created', checker)
+    bus.call('kattcmd:init', bus, folder=home)
+    assert checker.yay
+
+    checker = CallChecker()
+    bus.listen('kattcmd:open:problem-doesnt-exist', checker)
+    bus.call('kattcmd:open', bus, problemname)
+    assert checker.yay
+
+    should_be_empty1 = os.path.join(home, 'kattis', problemname)
+    should_be_empty2 = os.path.join(home, 'test', problemname)
+    assert not os.path.exists(should_be_empty1)
+    assert not os.path.exists(should_be_empty2)
