1f9fbc81913889f0092bffa5f15e433eca36a23d
Henke Adolfsson <catears@catears.se>
2018-03-11 11:14:41 +0100

    Clean up in main directory and guarantee Init before CLI

--- kattcmd/doc.py
+++
@@ -1,6 +0,0 @@
-
-Interactive = '''If the session should be interactive or not. Brings up the prompt.'''
-
-Command = '''What command to run, cannot be used with --interactive'''
-
-
--- kattcmd/main.py
+++ kattcmd/main.py
@@ -1,16 +1,6 @@
-import os
-import pydash
 import click
 
-from kattcmd import doc, bus, core
-
-
-def interactive_mode(bus, plugins):
-    pass
-
-
-def command_mode(bus, plugins, command):
-    pass
+from kattcmd import bus, core
 
 
 @click.group()
@@ -26,18 +16,12 @@ def main():
     plugins = core.ListPlugins()
     for plugin in plugins:
         plugin.Init(the_bus)
+
+    for plugin in plugins:
         if hasattr(plugin, 'CLI'):
             plugin.CLI(the_bus, cli_main)
 
     cli_main()
-
-    # # Check if interactive or not, if yes: run as interactive, if not then run command
-    # if interactive:
-    #     interactive_mode(the_bus, plugins)
-    # elif not command:
-    #     raise ValueError('Either you must specify a command or use interactive mode.')
-    # else:
-    #     command_mode(the_bus, plugins, command)
 
 
 if __name__ == '__main__':
